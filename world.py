32106ca0538cdba980b6af87e9db7796a94cbf80
Hiếu Trần <heraclex12@gmail.com>
2019-10-14 00:24:57 +0700

    add permutation for greedy and edit dijkstra and add return polygan points

--- world.py
+++ world.py
@@ -22,7 +22,7 @@ class World():
         self.stops = []
 
     def read_input(self):
-        with open("input.txt", "r") as file:
+        with open("input_2.txt", "r") as file:
             line = file.readline().strip("\s\n\r\t")
             self.leng, self.width = [int(i) + 1 for i in line.split(",")]
             for i in range(self.width):
@@ -36,8 +36,11 @@ class World():
                 self.area.append(row)
 
             line = file.readline().strip("\s\n\r\t")
-            p1, p2, p3, p4 = [int(i) for i in line.split(",")]
-            self.robot = Robot(p2, p1, p4, p3)
+            tmp = [int(i) for i in line.split(",")]
+            self.robot = Robot(tmp[1], tmp[0], tmp[3], tmp[2])
+            for j in range(4, len(tmp), 2):
+                self.stops.append((tmp[j + 1], tmp[j]))
+
             self.amount_polygan = file.readline().strip("\s\n\r\t")
             if self.amount_polygan is not None and line != "":
                 self.amount_polygan = int(self.amount_polygan)
@@ -50,35 +53,33 @@ class World():
 
                     self.polygans.append(polygan)
 
-            line = file.readline().strip("\s\n\r\t")
-            if line is not None and line != "":
-                tmp = [int(i) for i in line.split(",")]
-                for j in range(0, len(tmp), 2):
-                    self.area[tmp[j + 1]][tmp[j]] = "P"
-                    self.stops.append((tmp[j + 1], tmp[j]))
-
 
     def eucliean_distance(self, x1, y1, x2, y2):
         return (((x2 - x1) ** 2) + ((y2 - y1) ** 2)) ** 0.5
 
 
-    def drawing_polygan(self, polygan : list):
-        def match_two_point(point_a : tuple, point_b : tuple):
+    def drawing_polygan(self, polygan : list) -> list:
+        def match_two_point(point_a : tuple, point_b : tuple) -> list:
+            polygan_path = []
             if point_a[0] == point_b[0]:
                 for match in range(0, abs(point_a[1] - point_b[1]) + 1):
                     if point_b[1] > point_a[1]:
                         self.area[point_a[0]][point_a[1] + match] = "#"
+                        polygan_path.append((point_a[0], point_a[1] + match))
 
                     else:
                         self.area[point_a[0]][point_b[1] + match] = "#"
+                        polygan_path.append((point_a[0], point_b[1] + match))
 
             elif point_a[1] == point_b[1]:
                 for match in range(0, abs(point_a[0] - point_b[0]) + 1):
                     if point_b[0] > point_a[0]:
                         self.area[point_a[0] + match][point_a[1]] = "#"
+                        polygan_path.append((point_a[0] + match, point_a[1]))
 
                     else:
                         self.area[point_b[0] + match][point_b[1]] = "#"
+                        polygan_path.append((point_b[0] + match, point_b[1]))
 
             else:
                 positions = [(1, -1), (-1, 0), (-1, -1), (0, -1), (-1, 1), (1, 0), (0, 1), (1, 1)]
@@ -111,22 +112,75 @@ class World():
                     x_next = x_tmp
                     y_next = y_tmp
                     self.area[x_next][y_next] = "#"
+                    polygan_path.append((x_next, y_next))
+
+            return polygan_path
 
         self.area[polygan[0][0]][polygan[0][1]] = "#"
+        polygan_path = [(polygan[0][0], polygan[0][1])]
         for index in range(1, len(polygan)):
             curr = polygan[index]
             prev = polygan[index - 1]
-            match_two_point(prev, curr)
+            polygan_path.extend(match_two_point(prev, curr))
 
-        match_two_point(polygan[0], polygan[len(polygan) - 1])
+        polygan_path.extend(match_two_point(polygan[0], polygan[len(polygan) - 1]))
+        return polygan_path
 
 
     def greedy_search(self) -> list:
+        def find_permutation(k: int, min_c: int, perm: list, cost: int):
+            if k == 1:
+                if min_c > cost:
+                    min_c = cost
+                return
+            for i in range(k - 1):
+                find_permutation(k - 1, min_c, perm, cost)
+                if k % 2 == 0:
+                    perm[i], perm[k - 1] = perm[k - 1], perm[i]
+                    if i >= 1:
+                        cost -= self.eucliean_distance(perm[k - 1][0], perm[k - 1][1], perm[i + 1][0], perm[i + 1][1])
+                        cost -= self.eucliean_distance(perm[k - 1][0], perm[k - 1][1], perm[i - 1][0], perm[i - 1][1])
+                        cost += self.eucliean_distance(perm[i][0], perm[i][1], perm[i + 1][0], perm[i + 1][1])
+                        cost += self.eucliean_distance(perm[i][0], perm[i][1], perm[i - 1][0], perm[i - 1][1])
+                    else:
+                        cost -= self.eucliean_distance(perm[k - 1][0], perm[k - 1][1], perm[i + 1][0], perm[i + 1][1])
+                        cost += self.eucliean_distance(perm[i][0], perm[i][1], perm[i + 1][0], perm[i + 1][1])
+
+                    if k - 1 >= 1:
+                        cost -= self.eucliean_distance(perm[i][0], perm[i][1], perm[k][0], perm[k][1])
+                        cost += self.eucliean_distance(perm[k - 1][0], perm[k - 1][1], perm[k][0], perm[k][1])
+                        cost -= self.eucliean_distance(perm[i][0], perm[i][1], perm[k - 2][0], perm[k - 2][1])
+                        cost += self.eucliean_distance(perm[k - 1][0], perm[k - 1][1], perm[k - 2][0], perm[k - 2][1])
+                    else:
+                        cost -= self.eucliean_distance(perm[i][0], perm[i][1], perm[k][0], perm[k][1])
+                        cost += self.eucliean_distance(perm[k - 1][0], perm[k - 1][1], perm[k][0], perm[k][1])
+
+                else:
+                    perm[0], perm[k - 1] = perm[k - 1], perm[0]
+                    cost -= self.eucliean_distance(perm[k - 1][0], perm[k - 1][1], perm[1][0], perm[1][1])
+                    cost += self.eucliean_distance(perm[0][0], perm[0][1], perm[1][0], perm[1][1])
+                    if k - 1 >= 1:
+                        cost -= self.eucliean_distance(perm[0][0], perm[0][1], perm[k][0], perm[k][1])
+                        cost += self.eucliean_distance(perm[k - 1][0], perm[k - 1][1], perm[k][0], perm[k][1])
+                        cost -= self.eucliean_distance(perm[0][0], perm[0][1], perm[k - 2][0], perm[k - 2][1])
+                        cost += self.eucliean_distance(perm[k - 1][0], perm[k - 1][1], perm[k - 2][0], perm[k - 2][1])
+                    else:
+                        cost -= self.eucliean_distance(perm[0][0], perm[0][1], perm[k][0], perm[k][1])
+                        cost += self.eucliean_distance(perm[k - 1][0], perm[k - 1][1], perm[k][0], perm[k][1])
+
+            find_permutation(k - 1, min_c, perm, cost)
+
+
         robot_path = []
         positions = [(1, -1), (-1, 0), (-1, -1), (0, -1), (-1, 1), (1, 0), (0, 1), (1, 1)]
         start_point = self.robot.get_start_point()
         passing_points = []
         passing_points.extend(self.stops)
+        if len(passing_points) >= 2:
+            cost = self.eucliean_distance(start_point[0], start_point[1], passing_points[0][0], passing_points[0][1])
+            for i in range(0, len(passing_points) - 1):
+                cost += self.eucliean_distance(passing_points[i][0], passing_points[i][1], passing_points[i + 1][0], passing_points[i + 1][1])
+            find_permutation(len(passing_points) - 1, cost, passing_points, cost)
         passing_points.append(self.robot.get_end_point())
         while passing_points:
             minimum = self.leng * self.width
@@ -190,7 +244,7 @@ class World():
         queue_points.append(start_point)
         area_with_weight = copy.deepcopy(self.area)
         while queue_points:
-            curr_point = queue_points.pop()
+            curr_point = queue_points.pop(0)
             x_tmp = curr_point[0]
             y_tmp = curr_point[1]
             closed_points[x_tmp].add(y_tmp)
@@ -249,24 +303,25 @@ class World():
 
         return robot_path
 
-
-
     def print_area(self):
         start_point = self.robot.get_start_point()
         self.area[start_point[0]][start_point[1]] = "S"
         end_point = self.robot.get_end_point()
         self.area[end_point[0]][end_point[1]] = "G"
+        for i in self.stops:
+            self.area[i[0]][i[1]] = "P"
         for i in range(self.width - 1, -1, -1):
             for j in range(self.leng):
                 print(self.area[i][j], end=" ")
             print()
 
 
-world = World()
-world.read_input()
-for i in world.polygans:
-    world.drawing_polygan(i)
+if __name__ == '__main__':
+    world = World()
+    world.read_input()
+    for i in world.polygans:
+        world.drawing_polygan(i)
 
-# world.greedy_search()
-world.dijkstra_search()
-world.print_area()
+    world.greedy_search()
+    # world.dijkstra_search()
+    world.print_area()
